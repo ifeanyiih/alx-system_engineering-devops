11b1747cf1abe3bd638af34ea12d465a9ed2eb5a
IhillaryI <ifeanyichukwuhillary2019@gmail.com>
2023-01-31 16:12:19 +0000

    Add file start for 100-textme.rb
    Delete file 0-simply_match_school.rb, because of errs

--- 0x06-regular_expressions/0-simply_match_school.rb
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env ruby
-#Script contains the regular expression that will match a case(s)
-puts ARGV[0].scan(/School/).join
---
+++ 0x06-regular_expressions/100-textme.rb
@@ -0,0 +1,3 @@
+#!/usr/bin/env ruby
+#Script matches a regular expression
+puts ARGV[0].scan(/[A-Z]/).join
