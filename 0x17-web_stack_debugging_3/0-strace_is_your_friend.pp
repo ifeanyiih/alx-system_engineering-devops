8214c4fa88cc5e87e5c27fde2676907575f14592
Ifeanyichukwu Ifeanyichukwu <Ifeanyichukwuhillary2019@gmail.com>
2023-05-12 20:37:17 +0000

    modified

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,8 +1,8 @@
 # Automate apache2 fix
 
-file { '/var/www/html/index.html':
-ensure  => 'present',
-content => 'Hello, World!'
+exec { 'fix':
+provider => shell,
+command  => 'sed -i "s/phpp/php/g" /var/www/html/wp-settings.php'
 }
 
 
