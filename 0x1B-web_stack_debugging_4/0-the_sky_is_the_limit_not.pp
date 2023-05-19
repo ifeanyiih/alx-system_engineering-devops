36c7a7aaca8fbef7ac42dae4739ab3c1d053c1ca
Ifeanyichukwu Ifeanyichukwu <Ifeanyichukwuhillary2019@gmail.com>
2023-05-19 23:07:34 +0000

    modified

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,13 +1,13 @@
-# Automate apache2 fix
+# fix Too many files open error to enable nginx recieve 2000 request, 100 at a time
 
 exec { 'fix':
 provider => shell,
-command  => 'sed -i "s/ULIMIT.*/ULIMIT=\"-n 4096\"/" /etc/default/nginx'
+command  => 'sudo sed -i "s/ULIMIT.*/ULIMIT=\"-n 4096\"/" /etc/default/nginx'
 }
 
 exec {'restart':
 provider => shell,
-command  => 'service nginx restart'
+command  => 'sudo service nginx restart'
 }
 
 
